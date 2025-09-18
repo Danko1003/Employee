596d0770462cb7443dae747e711d4f952f7cd399
Daniel Koivunen <koivunendan10@gmail.com>
2025-09-18 13:53:34 -0400

    Fixed it again

--- Employee.java
+++ Employee.java
@@ -29,30 +29,31 @@ public class Employee{
     /**
      * Returns true is an employee is above 16 years old
      */
-    //public boolean canDrive() {
-        //int age = calculateAge(2025);
-        //if (age >= 16) {
-            //return true;
-
-        //} //else {
-            //System.out.println("you can drive in" + (16 - age) + "years");
-            //return false
-        //}
-    //}
+    public boolean canDrive() {
+        int age = calculateAge(2025);
+        if (age >= 16) {
+            System.out.println("you are eligible to drive");
+            return true;
+            
+        } else {
+            System.out.println("you can drive in" + (16 - age) + "years");
+            return false;
+        }
+    }
 
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
-    //private double calculatePay(){
+    private double calculatePay(){
         // to be completed
-        //basePay = hourlyWage*unpaidHours;
-        //taxe = 0.30;
-        //deductedPay = basePay*taxe;
-        //totalPay = basePay - deductedPay;
-        //return totalPay;
+        double basePay = hourlyWage*unpaidHours;
+        double taxe = 0.30;
+        double deductedPay = basePay*taxe;
+        double totalPay = basePay - deductedPay;
+        return totalPay;
         
         
-    //}
+    }
     
     public String getName() {
         return fullname;
@@ -67,8 +68,8 @@ public class Employee{
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
-        //amount = calculatePay();
-        //System.out.println(fullname + " has received a wire tranfer of " + amount + " CAD");
-        //unpaidHours = 0.0;
+        double amount = calculatePay();
+        System.out.println(fullname + " has received a wire tranfer of " + amount + " CAD");
+        unpaidHours = 0.0;
     }
 }
